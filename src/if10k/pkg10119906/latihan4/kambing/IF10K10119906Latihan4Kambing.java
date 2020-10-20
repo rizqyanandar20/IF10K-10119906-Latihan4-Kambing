0b4f0a3ed13aa55548a6a685e49ee3a6264d52cb
rizqyanandar20 <rizqyananda.rusmana@gmail.com>
2020-10-21 06:37:58 +0700

    first commit

---
+++ src/if10k/pkg10119906/latihan4/kambing/IF10K10119906Latihan4Kambing.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119906.latihan4.kambing;
+
+/**
+ *
+ * @author senenngahenen
+ * Nama      : Rizqy Ananda Rusmana
+ * NIM       : 10119906
+ * Kelas     : IF-10K
+ * Deskripsi : deklarasi Variable Lokal.
+ */
+public class IF10K10119906Latihan4Kambing {
+
+    /**
+     */
+    public void tambahKambing() {
+        // TODO code application logic here
+    
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah Kambing setelah penambahan " +jumlahKambing);
+    }
+    
+    public static void main(String[] args) {
+        IF10K10119906Latihan4Kambing kambingJantan = new IF10K10119906Latihan4Kambing();
+        kambingJantan.tambahKambing();
+    }        
+}
